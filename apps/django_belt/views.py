21a580bc23b80746e3e7f0cd0bdd7c6b50d26a45
Daryll Dade <Darylldade@yahoo.com>
2017-07-26 17:16:38 -0500

    Initial Commit

---
+++ apps/django_belt/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+
+
+
+#def flashErrors
+
+
+def index(request):
+
+    return render(request, 'django_belt/index.html')
+
+#def register
+
+#def login
+
+#def success
+
+#def logout
